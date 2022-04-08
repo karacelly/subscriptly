31090bb39dfc4997c806d0c95aed68cad483ea5d
Kesya Amanda <aracellykesya@gmail.com>
2022-04-08 23:29:50 +0700

    created login activity ui

---
+++ app/src/main/java/edu/bluejack21_2/subscriptly/SignUpActivity.java
@@ -0,0 +1,14 @@
+package edu.bluejack21_2.subscriptly;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
