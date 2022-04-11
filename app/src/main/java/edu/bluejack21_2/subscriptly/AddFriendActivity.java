16c4f2211859b4eb73cfc5699c8eb29f70006410
Kesya Amanda <aracellykesya@gmail.com>
2022-04-11 20:36:18 +0700

    make home fragment ui and add friend ui

---
+++ app/src/main/java/edu/bluejack21_2/subscriptly/AddFriendActivity.java
@@ -0,0 +1,37 @@
+package edu.bluejack21_2.subscriptly;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import edu.bluejack21_2.subscriptly.databinding.ActivityAddFriendBinding;
+
+public class AddFriendActivity extends AppCompatActivity {
+
+    private ActivityAddFriendBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityAddFriendBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_add_friend);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(binding.navView, navController);
+    }
+
+}
