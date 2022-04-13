198579f45c0a3b3b72bf58739dcd989159ce6df5
winstonkhoe <winstonkhoe@yahoo.com>
2022-04-13 23:29:06 +0700

    Added SearchView Listener add SearchFriend

--- app/src/main/java/edu/bluejack21_2/subscriptly/ui/friends/FriendsFragment.java
+++ app/src/main/java/edu/bluejack21_2/subscriptly/ui/friends/FriendsFragment.java
@@ -2,17 +2,21 @@ package edu.bluejack21_2.subscriptly.ui.friends;
 
 import android.os.Bundle;
 
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import javax.annotation.Nullable;
 
 import edu.bluejack21_2.subscriptly.R;
 
 
 public class FriendsFragment extends Fragment {
-
+    SearchView fieldSearchFriend;
     public FriendsFragment() {
         // Required empty public constructor
     }
@@ -29,6 +33,7 @@ public class FriendsFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
 //        if (getArguments() != null) {
 //            mParam1 = getArguments().getString(ARG_PARAM1);
 //            mParam2 = getArguments().getString(ARG_PARAM2);
@@ -43,5 +48,23 @@ public class FriendsFragment extends Fragment {
 
         return rootView;
 //        return inflater.inflate(R.layout.fragment_friends, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        fieldSearchFriend = getView().findViewById(R.id.field_search_friend);
+        fieldSearchFriend.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                Log.d("SearchFriend", newText);
+                return false;
+            }
+        });
     }
 }
