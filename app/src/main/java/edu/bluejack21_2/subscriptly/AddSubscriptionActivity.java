dd50fb197136a7a8d618c7567a05fc6860405aa5
winstonkhoe <winstonkhoe@yahoo.com>
2022-04-13 14:38:19 +0700

    Added Append Friend, Added Rupiah Format

--- app/src/main/java/edu/bluejack21_2/subscriptly/AddSubscriptionActivity.java
+++ app/src/main/java/edu/bluejack21_2/subscriptly/AddSubscriptionActivity.java
@@ -1,5 +1,21 @@
 package edu.bluejack21_2.subscriptly;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
 import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
@@ -8,44 +24,46 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.media.Image;
-import android.net.Uri;
-import android.os.Bundle;
-import android.provider.MediaStore;
-import android.util.Log;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.button.MaterialButton;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
+import java.text.NumberFormat;
+import java.util.Locale;
+
 public class AddSubscriptionActivity extends AppCompatActivity {
 
+    // Subscription Image
     CardView imageToggle;
     ImageView imageSubscription;
-    LinearLayout layoutList;
-    ImageButton buttonAdd, buttonSubmitList;
+
+    EditText subscriptionBill;
+
+    // Friend List
+    LinearLayout layoutFriendList;
+    ImageButton buttonAdd;
+    EditText friendName;
+    MaterialButton buttonCreateSubscription;
+
+    Context context;
+
+    Locale localeID = new Locale("in", "ID");
+    NumberFormat formatRupiah = NumberFormat.getCurrencyInstance(localeID);
+    private String current = "";
 
     private void initComponents() {
         imageToggle = findViewById(R.id.action_pick_image);
         imageSubscription = findViewById(R.id.image_subscription);
-        layoutList = findViewById(R.id.layout_list);
-        buttonAdd = findViewById(R.id.btn_add_friend);
 
-        buttonAdd.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                addView();
-            }
-        });
+        subscriptionBill = findViewById(R.id.field_subscription_bill);
+
+        layoutFriendList = findViewById(R.id.layout_friend_list);
+        buttonAdd = findViewById(R.id.action_add_friend);
+        friendName = findViewById(R.id.field_name_friend_default);
+        buttonCreateSubscription = findViewById(R.id.action_create_subscription);
     }
 
     @Override
@@ -55,72 +73,146 @@ public class AddSubscriptionActivity extends AppCompatActivity {
 
         initComponents();
 
+        context = this;
+
         imageToggle.setOnClickListener(v -> {
             Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
             pickImageActivityResultLauncher.launch(intent);
         });
-    }
 
-    ActivityResultLauncher<Intent> pickImageActivityResultLauncher = registerForActivityResult(
-        new ActivityResultContracts.StartActivityForResult(),
-        new ActivityResultCallback<ActivityResult>() {
+        buttonAdd.setOnClickListener(v -> {
+            addView();
+        });
+
+        subscriptionBill.addTextChangedListener(new TextWatcher() {
             @Override
-            public void onActivityResult(ActivityResult result) {
-//                if(result.getResultCode() == 3) {
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            //            @Override
+//            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 //
-//                }
-                if (result.getResultCode() == Activity.RESULT_OK) {
-                    // There are no request codes
-                    Intent data = result.getData();
+//            }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (!s.toString().equals(current)) {
+                    subscriptionBill.removeTextChangedListener(this);
 
-                    if(data != null) {
-                        Uri selectedImage = data.getData();
-                        imageSubscription.setImageURI(selectedImage);
-                        Log.d("Image URI", selectedImage.toString());
-//                      doSomeOperations();
-                        FirebaseStorage storage = FirebaseStorage.getInstance();
-                        StorageReference storageRef = storage.getReference();
-                        StorageReference subscriptionStorageRef = storageRef.child("subscriptions/"+selectedImage.getLastPathSegment());
-                        UploadTask uploadTask;
-                        uploadTask = subscriptionStorageRef.putFile(selectedImage);
+                    String cleanString = s.toString().replaceAll("[Rp,.]", "");
+                    Log.d("Clean String", cleanString);
+                    Integer parsed = Integer.parseInt(cleanString);
 
-// Register observers to listen for when the download is done or if it fails
-                        uploadTask.addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception exception) {
-                                // Handle unsuccessful uploads
-                                Log.d("Upload", exception.toString());
-                            }
-                        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                            @Override
-                            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                                // taskSnapshot.getMetadata() contains file metadata such as size, content-type, etc.
-                                // ...
-                                Log.d("Upload", taskSnapshot.getMetadata().toString());
-                            }
-                        });
+                    String formatted = formatRupiah.format(parsed);
+
+                    current = formatted;
+                    Log.d("Formatted String", formatted);
+                    subscriptionBill.setText(formatted);
+                    subscriptionBill.setSelection(formatted.length());
+                    subscriptionBill.addTextChangedListener(this);
+                }
+                if(!s.toString().matches("^\\$(\\d{1,3}(\\,\\d{3})*|(\\d+))(\\.\\d{2})?$"))
+                {
+                    String userInput= ""+s.toString().replaceAll("[^\\d]", "");
+                    if (userInput.length() > 0) {
+                        Integer in= Integer.parseInt(userInput);
+                        subscriptionBill.setText("Rp"+formatRupiah.format(in));
+                        subscriptionBill.setSelection(subscriptionBill.getText().length());
                     }
                 }
             }
-    });
 
-    private void addView() {
-        final View friendView = getLayoutInflater().inflate(R.layout.activity_add_subscription, null, false);
 
-        EditText editText = (EditText) friendView.findViewById(R.id.edit_text_friend);
-        ImageButton imgBtn = (ImageButton) friendView.findViewById(R.id.btn_delete_friend);
 
-        imgBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                removeView(friendView);
+            public void afterTextChanged(Editable editable) {
+
             }
         });
 
-        layoutList.addView(friendView);
+        buttonCreateSubscription.setOnClickListener(v -> {
+            for (int i = 0; i < layoutFriendList.getChildCount(); i++) {
+                if (layoutFriendList.getChildAt(i) instanceof LinearLayout) {
+                    LinearLayout row = (LinearLayout) layoutFriendList.getChildAt(i);
+                    for (int j = 0; j < row.getChildCount(); j++) {
+                        if (row.getChildAt(j) instanceof EditText) {
+                            EditText friendName = (EditText) row.getChildAt(j);
+                            if (friendName.getId() == R.id.field_name_friend) {
+                                Toast.makeText(context, "" + friendName.getText().toString(), Toast.LENGTH_SHORT);
+                            }
+                            Log.d("FRIEND NAME", friendName.getText().toString());
+                        }
+                    }
+                }
+            }
+        });
+
+    }
+
+    ActivityResultLauncher<Intent> pickImageActivityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+//                if(result.getResultCode() == 3) {
+//
+//                }
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        // There are no request codes
+                        Intent data = result.getData();
+
+                        if (data != null) {
+                            Uri selectedImage = data.getData();
+                            imageSubscription.setImageURI(selectedImage);
+                            Log.d("Image URI", selectedImage.toString());
+//                      doSomeOperations();
+                            FirebaseStorage storage = FirebaseStorage.getInstance();
+                            StorageReference storageRef = storage.getReference();
+                            StorageReference subscriptionStorageRef = storageRef.child("subscriptions/" + selectedImage.getLastPathSegment());
+                            UploadTask uploadTask;
+                            uploadTask = subscriptionStorageRef.putFile(selectedImage);
+
+// Register observers to listen for when the download is done or if it fails
+                            uploadTask.addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception exception) {
+                                    // Handle unsuccessful uploads
+                                    Log.d("Upload", exception.toString());
+                                }
+                            }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                                @Override
+                                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                                    // taskSnapshot.getMetadata() contains file metadata such as size, content-type, etc.
+                                    // ...
+                                    Log.d("Upload", taskSnapshot.getMetadata().toString());
+                                }
+                            });
+                        }
+                    }
+                }
+            });
+
+
+    private void addView() {
+        String friendNameContent = friendName.getText().toString().trim();
+        if (friendNameContent.length() > 0) {
+            final View friendView = getLayoutInflater().inflate(R.layout.row_add_friend, null, false);
+
+            EditText friendNameAppend = friendView.findViewById(R.id.field_name_friend);
+            ImageButton removeFriend = friendView.findViewById(R.id.action_remove_friend);
+
+            friendNameAppend.setText(friendNameContent);
+            removeFriend.setOnClickListener(v -> {
+                removeView(friendView);
+            });
+
+            layoutFriendList.addView(friendView, 0);
+//            layoutFriendList.addView(friendView);
+        }
+        friendName.setText("");
     }
 
     private void removeView(View v) {
-        layoutList.removeView(v);
+        layoutFriendList.removeView(v);
     }
 }
