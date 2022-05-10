03946ebefefd17dfdaf8fadc2f32afaf2d3ac06b
Kesya Amanda <aracellykesya@gmail.com>
2022-05-11 01:36:43 +0700

    create pay subscription ui

---
+++ app/src/main/java/edu/bluejack21_2/subscriptly/PaySubscriptionActivity.java
@@ -0,0 +1,14 @@
+package edu.bluejack21_2.subscriptly;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PaySubscriptionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pay_subscription);
+    }
+}
