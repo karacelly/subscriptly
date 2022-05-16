8e9690a612966744ae502ebf66c874c0f54432c9
Kesya Amanda <aracellykesya@gmail.com>
2022-05-16 15:29:03 +0700

    on progress notification

---
+++ app/src/main/java/edu/bluejack21_2/subscriptly/adapter/NotificationAdapter.java
@@ -0,0 +1,30 @@
+package edu.bluejack21_2.subscriptly.adapter;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import edu.bluejack21_2.subscriptly.R;
+
+public class NotificationAdapter extends Fragment {
+
+    public NotificationAdapter() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.adapter_notification, container, false);
+    }
+}
---
+++ app/src/main/java/edu/bluejack21_2/subscriptly/adapter/viewholder/NotificationViewHolder.java
@@ -0,0 +1,26 @@
+package edu.bluejack21_2.subscriptly.adapter.viewholder;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import edu.bluejack21_2.subscriptly.databinding.AdapterNotificationBinding;
+import edu.bluejack21_2.subscriptly.models.Subscription;
+import edu.bluejack21_2.subscriptly.models.TransactionDetail;
+
+public class NotificationViewHolder  extends RecyclerView.ViewHolder {
+    private final AdapterNotificationBinding binding;
+    private final View view;
+
+    public NotificationViewHolder(AdapterNotificationBinding binding) {
+        super(binding.getRoot());
+        view = binding.getRoot();
+        this.binding = binding;
+    }
+
+    public void bind(TransactionDetail detail, Subscription subscription){
+        binding.setUser(detail.getUser());
+        binding.setSubscription(subscription);
+    }
+}
